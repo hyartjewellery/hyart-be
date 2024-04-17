181db0ef52e929afd447a8aaef832c67cf219857
Supragya2912 <anandsupragya@gmail.com>
2024-04-18 01:36:47 +0530

    updated url

--- controllers/auth.js
+++ controllers/auth.js
@@ -244,7 +244,7 @@ const forgotPassword = async (req, res) => {
             { new: true }
         );
 
-        const url = `http://localhost:3000/update-password/${token}`;
+        const url = `http://localhost:3000/reset-password/${token}`;
         const message = `Click on the link below to reset your password:\n\n${url}`;
 
         await mailSender(
