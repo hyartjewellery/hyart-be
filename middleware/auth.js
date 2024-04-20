97bb387aa8bff70e4ec434e74cae0b3e7eecea91
Satyam <satyam19arya@gmail.com>
2024-04-20 20:36:18 +0530

    updated

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,13 +1,11 @@
 const jwt = require('jsonwebtoken');
+const { error } = require('../utils/responseWrapper');
 const User = require('../models/User');
 
 exports.protect = async (req, res, next) => {
 
     if(!req.headers || !req.headers.authorization || !req.headers.authorization.startsWith("Bearer")){
-        return res.status(401).json({
-            success: false,
-            message: 'Authorization header is required'
-        })
+        return res.send(error(401, 'Authorization header is required'));
     }
 
     const accessToken = req.headers.authorization.split(" ")[1];
@@ -16,19 +14,12 @@ exports.protect = async (req, res, next) => {
         const decoded = jwt.verify(accessToken, process.env.ACCESS_TOKEN_PRIVATE_KEY);
         const user = await User.findById(decoded._id);
         if(!user){
-            return res.status(404).json({
-                success: false,
-                message: 'User not found'
-            })
+            return res.send(error(404, 'User not found'));
         }
         req.user = user;
         next();
     } catch(err){
         console.log(err);
-        return res.status(401).json({
-            success: false,
-            message: 'Token verification failed',
-            error: err.message
-        })
+        return res.send(error(401, 'Invalid access key'));
     }
 }
